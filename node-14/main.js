6eabbb0b61503d9bfcc41158fc5758f71313265d
Seanghay Yath <seanghay.dev@gmail.com>
2022-07-04 15:38:31 +0700

    fix: node version

--- node-14/main.js
+++ node-14/main.js
@@ -1,4 +1,4 @@
 import _ from 'lodash'
 const collection = ['Hello', 'HELLO']
-console.log(`${_.sample(collection)} from Node.js 16.`)
+console.log(`${_.sample(collection)} from Node.js 14.`)
 console.log(process.env)
