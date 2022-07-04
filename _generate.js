9c5304ed9e9b9cce98a8bc2a9f4d3d17ac7bb125
Seanghay Yath <seanghay.dev@gmail.com>
2022-07-04 15:21:02 +0700

    chore: add precommit hook

--- _generate.js
+++ _generate.js
@@ -2,10 +2,13 @@
 import fg from "fast-glob";
 import fs from "fs/promises";
 import path from "path";
+import mkdirp from "mkdirp";
 
 const workflowDir = path.resolve(".github/workflows/");
 const template = await fs.readFile("_ci-template.yml", "utf-8");
 const files = await fg("**/*/*Dockerfile");
+
+await mkdirp(workflowDir);
 
 for (const file of files) {
   const p = path.parse(file);
